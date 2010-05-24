4877b70a594729374b862273d472c471d9671c3c
Sean Cleveland <sean.b.cleveland@gmail.com>
2010-05-24 14:13:28 -0600

    Got JQuery to work

--- header.php
+++ header.php
@@ -23,6 +23,8 @@
   } ?>
   <title><? pageTitle() ?></title>
   <link rel="stylesheet" type="text/css" href="<? bloginfo('stylesheet_url')?>" media="screen" />
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js"></script>
   <? wp_head() ?>
 </head>
 
